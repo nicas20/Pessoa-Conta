0e0aacf553e90f4e2a7434f1bf3efe1cfeff61bf
nicas20 <100000527+nicas20@users.noreply.github.com>
2022-11-09 21:40:51 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+    private String nome;
+    private String altura;
+    private String idade;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+}
